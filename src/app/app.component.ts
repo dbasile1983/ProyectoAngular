6bf868e0bc933b5ee44eab66efc54c2568a70540
dbasile1983 <61365974+dbasile1983@users.noreply.github.com>
2023-05-26 11:56:33 -0300

    Se agrega un nuevo componente denominado Home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   standalone: true,
+  imports:[HomeComponent],
   selector: 'app-root',
-  template: `<h1>Mundial Juvenil SUB-20!</h1>`,
+  template: '<main><header class="brand-name"> <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true"> </header> <section class="content"> <app-home></app-home> </section> </main>',
   styleUrls: ['./app.component.css'],
-})
+}) 
 export class AppComponent {
   title = 'homes';
 }
